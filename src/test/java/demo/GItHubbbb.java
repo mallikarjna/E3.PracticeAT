c94491d696e7e3f914a498dac5257fd8869ee67c
91766 <91766@ROHITH>
2025-09-24 12:27:58 +0530

    fine

---
+++ src/test/java/demo/GItHubbbb.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class GItHubbbb {
+
+	public static void main(String[] args) {
+	System.out.println("what about you are head");
+
+	}
+
+}
--- src/test/java/demo/Simple.java
+++ src/test/java/demo/Simple.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("Hii");
 	
 	System.out.println("are you fine ra");
+	System.out.println("edho ala unna mawa");
 }
 		
 
